4723dc380637e096a2d7bf730da5718010d6e253
Kiran-Chaitu <22A91A05D0@aec.edu.in>
2026-05-03 09:17:48 +0530

    video added

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,6 +1,3 @@
-import cloudinary from "../config/cloudinary.js";
-import asyncHandler from "../utils/asyncHandler.js";
-
 export const uploadFile = asyncHandler(async (req, res) => {
   if (!req.file) {
     return res.status(400).json({
@@ -9,33 +6,27 @@ export const uploadFile = asyncHandler(async (req, res) => {
     });
   }
 
-  const file = req.file;
-
-  const isPDF = file.mimetype === "application/pdf";
-
   const result = await new Promise((resolve, reject) => {
-  const stream = cloudinary.uploader.upload_stream(
-    {
-      folder: "drawings",
-      resource_type: "auto", // 🔥 FIX
-      public_id: `file_${Date.now()}`,
-    },
-    (error, result) => {
-      if (error) reject(error);
-      else resolve(result);
-    }
-  );
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        folder: "drawings",
+        resource_type: "auto", // 🔥 key line
+        public_id: `file_${Date.now()}`,
+      },
+      (error, result) => {
+        if (error) reject(error);
+        else resolve(result);
+      }
+    );
 
-  stream.end(file.buffer);
-});
+    stream.end(req.file.buffer);
+  });
 
   return res.status(200).json({
     success: true,
-    message: "File uploaded successfully",
     data: {
       url: result.secure_url,
-      public_id: result.public_id,
-      type: result.resource_type,
+      type: result.resource_type, // 👈 useful for frontend
     },
   });
 });
--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -6,16 +6,19 @@ const storage = multer.memoryStorage();
 // File filter (only image + pdf)
 const fileFilter = (req, file, cb) => {
   const allowedTypes = [
-    "image/png",
-    "image/jpeg",
-    "image/jpg",
-    "application/pdf",
-  ];
+  "image/png",
+  "image/jpeg",
+  "image/jpg",
+  "application/pdf",
+  "video/mp4",
+  "video/quicktime", // .mov
+  "video/x-matroska", // .mkv (optional)
+];
 
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Only images and PDFs are allowed"), false);
+    cb(new Error("Only images, pdfs and videos are allowed"), false);
   }
 };
 
